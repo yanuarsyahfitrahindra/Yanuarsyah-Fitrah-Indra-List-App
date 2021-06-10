8fc631bd2c0266caac43b2665c696bc44b487fc3
Yanuarsyah Fitrah Indra <ryanindraaa@gmail.com>
2021-06-10 16:56:49 +0800

    Ubah dan perbaiki main activity

--- app/src/main/java/id/ac/unhas/listapp/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/listapp/MainActivity.kt
@@ -1,13 +1,28 @@
 package id.ac.unhas.listapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.BaseAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.barang.view.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.w3c.dom.Text
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    private var listBarang = ArrayList<Barang>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +30,106 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            var intent = Intent(this, BarangActivity::class.java)
+            startActivity(intent)
+        }
+
+        loadData()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadData()
+    }
+
+
+    private fun loadData() {
+        var dbAdapter = DBAdapter(this)
+        var cursor = dbAdapter.allQuery()
+
+        listBarang.clear()
+        if(cursor.moveToFirst()) {
+            do {
+                val id = cursor.getInt(cursor.getColumnIndex("Id"))
+                val nama = cursor.getString(cursor.getColumnIndex("Nama"))
+                val jumlah = cursor.getString(cursor.getColumnIndex("Jumlah"))
+                val harga = cursor.getString(cursor.getColumnIndex("Harga"))
+
+                listBarang.add(Barang(id, nama, jumlah, harga))
+            } while (cursor.moveToNext())
+        }
+        var barangAdapter = BarangAdapter(this, listBarang)
+        lvBarang.adapter = barangAdapter
+    }
+
+    inner class BarangAdapter: BaseAdapter{
+
+        private var barangList = ArrayList<Barang>()
+        private var context: Context? = null
+
+        constructor(context: Context, barangList: ArrayList<Barang>) : super() {
+            this.barangList = barangList
+            this.context = context
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+            val view: View?
+            val vh: ViewHolder
+
+            if (convertView == null){
+                view = layoutInflater.inflate(R.layout.barang, parent, false)
+                vh = ViewHolder(view)
+                view.tag = vh
+                Log.i("db", "set tag for ViewHolder, position: " + position)
+            } else {
+                view = convertView
+                vh = view.tag as ViewHolder
+            }
+
+            var mBarang = barangList[position]
+
+            vh.tvNama.text = mBarang.name
+            vh.tvJumlah.text = mBarang.jumlah
+            vh.tvHarga.text = "Rp." + mBarang.harga
+
+            lvBarang.onItemClickListener = AdapterView.OnItemClickListener {adapterView, viwe, position, id ->
+                updateBarang(mBarang)
+            }
+            return view
+        }
+
+        override fun getItem(position: Int): Any {
+            return barangList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return barangList.size
+        }
+
+    }
+
+    private fun updateBarang(barang: Barang) {
+        var intent = Intent (this, BarangActivity::class.java)
+        intent.putExtra("MainActId", barang.id)
+        intent.putExtra("MainActNama", barang.name)
+        intent.putExtra("MainActJumlah", barang.jumlah)
+        intent.putExtra("MainActHarga", barang.harga)
+        startActivity(intent)
+    }
+
+    private class ViewHolder(view: View?){
+        val tvNama: TextView
+        val tvJumlah: TextView
+        val tvHarga: TextView
+
+        init {
+            this.tvNama = view?.findViewById(R.id.tvNama) as TextView
+            this.tvJumlah = view?.findViewById(R.id.tvJumlah) as TextView
+            this.tvHarga = view?.findViewById(R.id.tvHarga) as TextView
         }
     }
 
